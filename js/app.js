ef358e157b19f8dabcff430612f3ffd6b8ae10c8
LauraJH16 <laurajimenezh16@hotmail.com>
2017-12-01 17:35:08 -0500

    Reduciendo codigo

--- js/app.js
+++ js/app.js
@@ -134,73 +134,71 @@ window.addEventListener('click', function() {
   var sedes = data[selectSede()[0]];
   var generation = sedes[selectSede()[1]];
   var students = generation['students'];
+  var ratings = generation.ratings;
   var boxActivas = document.getElementById('activas');
-  boxActivas.textContent = active(selectSede()[0], selectSede()[1]);
+  boxActivas.textContent = active();
   var boxDesactive = document.getElementById('desertoras');
-  boxDesactive.textContent = ((deserter(selectSede()[0], selectSede()[1]) / students.length) * 100).toFixed(1) + '%';
+  boxDesactive.textContent = ((deserter() / students.length) * 100).toFixed(1) + '%';
   var boxGoals = document.getElementById('goals');
-  boxGoals.textContent = goals(selectSede()[0], selectSede()[1]);
+  boxGoals.textContent = goals();
   var boxGoalsPorc = document.getElementById('goalsPorc');
-  boxGoalsPorc.textContent = ((goals(selectSede()[0], selectSede()[1]) / students.length) * 100).toFixed(1) + '%';
+  boxGoalsPorc.textContent = ((goals() / students.length) * 100).toFixed(1) + '%';
   var boxNps = document.getElementById('prom-nps');
-  boxNps.textContent = npsProm(selectSede()[0], selectSede()[1]).toFixed(1) + '%';
+  boxNps.textContent = npsProm().toFixed(1) + '%';
   var boxPromoter = document.getElementById('promoter');
-  boxPromoter.textContent = 'promoters: ' + promoterNps(selectSede()[0], selectSede()[1]).toFixed(1) + '%';
+  boxPromoter.textContent = 'promoters: ' + promoterNps().toFixed(1) + '%';
   var boxPassive = document.getElementById('passive');
-  boxPassive.textContent = 'passive: ' + passiveNps(selectSede()[0], selectSede()[1]).toFixed(1) + '%';
+  boxPassive.textContent = 'passive: ' + passiveNps().toFixed(1) + '%';
   var boxDetractor = document.getElementById('detractor');
-  boxDetractor.textContent = 'detractors: ' + detractorNps(selectSede()[0], selectSede()[1]).toFixed(1) + '%';
+  boxDetractor.textContent = 'detractors: ' + detractorNps().toFixed(1) + '%';
   var selectSprintTech = document.getElementById('tech-sprint');
   var boxTech = document.getElementById('skills-tech');
   var boxPercentTech = document.getElementById('tech-percent');
   if (selectSprintTech.value === 'sprint-1') {
-    boxTech.textContent = skillsTech(selectSede()[0], selectSede()[1])[0];
-    boxPercentTech.textContent = ((skillsTech(selectSede()[0], selectSede()[1])[0] / students.length) * 100).toFixed(1) + '%';
+    boxTech.textContent = skillsTech()[0];
+    boxPercentTech.textContent = ((skillsTech()[0] / students.length) * 100).toFixed(1) + '%';
   }
   if (selectSprintTech.value === 'sprint-2') {
-    boxTech.textContent = skillsTech(selectSede()[0], selectSede()[1])[1];
-    boxPercentTech.textContent = ((skillsTech(selectSede()[0], selectSede()[1])[1] / students.length) * 100).toFixed(1) + '%';
+    boxTech.textContent = skillsTech()[1];
+    boxPercentTech.textContent = ((skillsTech()[1] / students.length) * 100).toFixed(1) + '%';
   }
   if (selectSprintTech.value === 'sprint-3') {
-    boxTech.textContent = skillsTech(selectSede()[0], selectSede()[1])[2];
-    boxPercentTech.textContent = ((skillsTech(selectSede()[0], selectSede()[1])[2] / students.length) * 100).toFixed(1) + '%';
+    boxTech.textContent = skillsTech()[2];
+    boxPercentTech.textContent = ((skillsTech()[2] / students.length) * 100).toFixed(1) + '%';
   }
   if (selectSprintTech.value === 'sprint-4') {
-    boxTech.textContent = skillsTech(selectSede()[0], selectSede()[1])[3];
-    boxPercentTech.textContent = ((skillsTech(selectSede()[0], selectSede()[1])[3] / students.length) * 100).toFixed(1) + '%';
+    boxTech.textContent = skillsTech()[3];
+    boxPercentTech.textContent = ((skillsTech()[3] / students.length) * 100).toFixed(1) + '%';
   }
   var selectSprintHse = document.getElementById('hse-sprint');
   var boxHse = document.getElementById('skills-hse');
   var boxPercentHse = document.getElementById('hse-percent');
   if (selectSprintHse.value === 'sprint-1') {
-    boxHse.textContent = skillsHse(selectSede()[0], selectSede()[1])[0];
-    boxPercentHse.textContent = ((skillsHse(selectSede()[0], selectSede()[1])[0] / students.length) * 100).toFixed(1) + '%';
+    boxHse.textContent = skillsHse()[0];
+    boxPercentHse.textContent = ((skillsHse()[0] / students.length) * 100).toFixed(1) + '%';
   }
   if (selectSprintHse.value === 'sprint-2') {
-    boxHse.textContent = skillsHse(selectSede()[0], selectSede()[1])[1];
-    boxPercentHse.textContent = ((skillsHse(selectSede()[0], selectSede()[1])[1] / students.length) * 100).toFixed(1) + '%';
+    boxHse.textContent = skillsHse()[1];
+    boxPercentHse.textContent = ((skillsHse()[1] / students.length) * 100).toFixed(1) + '%';
   }
   if (selectSprintHse.value === 'sprint-3') {
-    boxHse.textContent = skillsHse(selectSede()[0], selectSede()[1])[2];
-    boxPercentHse.textContent = ((skillsHse(selectSede()[0], selectSede()[1])[2] / students.length) * 100).toFixed(1) + '%';
+    boxHse.textContent = skillsHse()[2];
+    boxPercentHse.textContent = ((skillsHse()[2] / students.length) * 100).toFixed(1) + '%';
   }
   if (selectSprintHse.value === 'sprint-4') {
-    boxHse.textContent = skillsHse(selectSede()[0], selectSede()[1])[3];
-    boxPercentHse.textContent = ((skillsHse(selectSede()[0], selectSede()[1])[3] / students.length) * 100).toFixed(1) + '%';
+    boxHse.textContent = skillsHse()[3];
+    boxPercentHse.textContent = ((skillsHse()[3] / students.length) * 100).toFixed(1) + '%';
   }
   var boxSatisfaction = document.getElementById('satisfaction-percent');
-  boxSatisfaction.textContent = studentSatisfaction(selectSede()[0], selectSede()[1]).toFixed(1) + '%';
+  boxSatisfaction.textContent = studentSatisfaction().toFixed(1) + '%';
   var jediPoints = document.getElementById('jedi');
-  jediPoints.textContent = pointsJedi(selectSede()[0], selectSede()[1]).toFixed(1);
+  jediPoints.textContent = pointsJedi().toFixed(1);
   var teacherPoints = document.getElementById('teacher');
-  teacherPoints.textContent = pointsTeacher(selectSede()[0], selectSede()[1]).toFixed(1);
-  studentPerfil(selectSede()[0], selectSede()[1]);
+  teacherPoints.textContent = pointsTeacher().toFixed(1);
+  studentPerfil();
 
-  function active(sedeValue, generationValue) {
+  function active() {
     var studentActive = 0;
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var students = generation['students'];
     for (var i = 0; i < students.length; i++) {
       if (students[i]['active']) {
         studentActive++;
@@ -209,12 +207,8 @@ window.addEventListener('click', function() {
     return studentActive;
   }
 
-  function deserter(sedeValue, generationValue) {
+  function deserter() {
     var studentDesert = 0;
-    var boxActives = document.getElementById('activas');
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var students = generation['students'];
     for (var i = 0; i < students.length; i++) {
       if (!students[i]['active']) {
         studentDesert++;
@@ -223,26 +217,18 @@ window.addEventListener('click', function() {
     return studentDesert;
   }
 
-  function goals(sedeValue, generationValue) {
+  function goals() {
     var studentsGoals = [];
-    var goalAchieved = 0;
-    var pointsTech = 0;
-    var pointsHse = 0;
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var students = generation['students'];
-
+    var goalAchieved = 0, pointsTech = 0, pointsHse = 0;
     for (var i = 0; i < students.length; i++) {
-      if (students[i].active) {
-        for (var j = 0; j < students[i]['sprints'].length; j++) {
-          pointsTech += students[i].sprints[j].score.tech;
-          pointsHse += students[i].sprints[j].score.hse;
-        }
-        var promTech = pointsTech / students[i].sprints.length;
-        var promHse = pointsHse / students[i].sprints.length;
-        var promTotal = promTech + promHse;
-        studentsGoals.push([promTotal]);
+      for (var j = 0; j < students[i]['sprints'].length; j++) {
+        pointsTech += students[i].sprints[j].score.tech;
+        pointsHse += students[i].sprints[j].score.hse;
       }
+      var promTech = pointsTech / students[i]['sprints'].length;
+      var promHse = pointsHse / students[i]['sprints'].length;
+      var promTotal = promTech + promHse;
+      studentsGoals.push([promTotal]);
     }
     for (var k = 0; k < studentsGoals.length; k++) {
       if (studentsGoals[k] >= 2100) {
@@ -252,10 +238,7 @@ window.addEventListener('click', function() {
     return goalAchieved;
   }
 
-  function npsProm(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var ratings = generation.ratings;
+  function npsProm() {
     var nps = 0;
     var promNps = 0;
     for (var i = 0; i < ratings.length; i++) {
@@ -265,12 +248,8 @@ window.addEventListener('click', function() {
     return promNps;
   }
 
-  function promoterNps(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var ratings = generation.ratings;
+  function promoterNps() {
     var promotersPercent = 0;
-
     for (i = 0; i < ratings.length; i++) {
       var totalNps = ratings[i].nps.promoters + ratings[i].nps.passive + ratings[i].nps.detractors;
       promotersPercent += ((ratings[i].nps.promoters) / totalNps) * 100;
@@ -279,12 +258,8 @@ window.addEventListener('click', function() {
     return promPromotersPercent;
   }
 
-  function passiveNps(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var ratings = generation.ratings;
+  function passiveNps() {
     var passivePercent = 0;
-
     for (i = 0; i < ratings.length; i++) {
       var totalNps = ratings[i].nps.promoters + ratings[i].nps.passive + ratings[i].nps.detractors;
       passivePercent += ((ratings[i].nps.passive) / totalNps) * 100;
@@ -293,12 +268,8 @@ window.addEventListener('click', function() {
     return promPassivePercent;
   }
 
-  function detractorNps(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var ratings = generation.ratings;
+  function detractorNps() {
     var detractorPercent = 0;
-
     for (i = 0; i < ratings.length; i++) {
       var totalNps = ratings[i].nps.promoters + ratings[i].nps.passive + ratings[i].nps.detractors;
       detractorPercent += ((ratings[i].nps.detractors) / totalNps) * 100;
@@ -307,63 +278,53 @@ window.addEventListener('click', function() {
     return promDetractorPercent;
   }
 
-  function skillsTech(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var students = generation['students'];
+  function skillsTech() {
     var arrayTech = [];
-    var sprint1 = 0,
-      sprint2 = 0,
-      sprint3 = 0,
-      sprint4 = 0;
+    var sprint1 = 0, sprint2 = 0, sprint3 = 0, sprint4 = 0;  
     for (i = 0; i < students.length; i++) {
-      if (students[i].active) {
-        for (k = 0; k < students[i].sprints.length; k++) {
-          if (students[i].sprints[k].number === 1) {
-            if (students[i].sprints[k].score.tech >= 1260)
-              sprint1++;
-          } else if (students[i].sprints[k].number === 2) {
-            if (students[i].sprints[k].score.tech >= 1260)
-              sprint2++;
-          } else if (students[i].sprints[k].number === 3) {
-            if (students[i].sprints[k].score.tech >= 1260)
-              sprint3++;
-          } else if (students[i].sprints[k].number === 4) {
-            if (students[i].sprints[k].score.tech >= 1260)
-              sprint4++;
-          }
+      for (k = 0; k < students[i].sprints.length; k++) {
+        if (students[i].sprints[k].number === 1) {
+          if (students[i].sprints[k].score.tech >= 1260)
+            sprint1++;
+        }
+        if (students[i].sprints[k].number === 2) {
+          if (students[i].sprints[k].score.tech >= 1260)
+            sprint2++;
+        } 
+        if (students[i].sprints[k].number === 3) {
+          if (students[i].sprints[k].score.tech >= 1260)
+            sprint3++;
+        }
+        if (students[i].sprints[k].number === 4) {
+          if (students[i].sprints[k].score.tech >= 1260)
+            sprint4++;
         }
       }
     }
-    arrayTech.push(sprint1, sprint2, sprint3, sprint4);
+    arrayTech.push(sprint1, sprint2, sprint3, sprint4); 
     return arrayTech;
   }
 
-  function skillsHse(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var students = generation['students'];
+  function skillsHse() {
     var arrayHse = [];
-    var sprint1 = 0,
-      sprint2 = 0,
-      sprint3 = 0,
-      sprint4 = 0;
+    var sprint1 = 0, sprint2 = 0, sprint3 = 0, sprint4 = 0;
     for (i = 0; i < students.length; i++) {
-      if (students[i].active) {
-        for (k = 0; k < students[i].sprints.length; k++) {
-          if (students[i].sprints[k].number === 1) {
-            if (students[i].sprints[k].score.hse >= 840)
-              sprint1++;
-          } else if (students[i].sprints[k].number === 2) {
-            if (students[i].sprints[k].score.hse >= 840)
-              sprint2++;
-          } else if (students[i].sprints[k].number === 3) {
-            if (students[i].sprints[k].score.hse >= 840)
-              sprint3++;
-          } else if (students[i].sprints[k].number === 4) {
-            if (students[i].sprints[k].score.hse >= 840)
-              sprint4++;
-          }
+      for (k = 0; k < students[i].sprints.length; k++) {
+        if (students[i].sprints[k].number === 1) {
+          if (students[i].sprints[k].score.hse >= 840)
+            sprint1++;
+        }
+        if (students[i].sprints[k].number === 2) {
+          if (students[i].sprints[k].score.hse >= 840)
+            sprint2++;
+        }
+        if (students[i].sprints[k].number === 3) {
+          if (students[i].sprints[k].score.hse >= 840)
+            sprint3++;
+        }
+        if (students[i].sprints[k].number === 4) {
+          if (students[i].sprints[k].score.hse >= 840)
+            sprint4++;
         }
       }
     }
@@ -371,10 +332,7 @@ window.addEventListener('click', function() {
     return arrayHse;
   }
 
-  function studentSatisfaction(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var ratings = generation.ratings;
+  function studentSatisfaction() {
     var satisfaction = 0;
     var promSatisfaction = 0;
     for (var i = 0; i < ratings.length; i++) {
@@ -384,10 +342,7 @@ window.addEventListener('click', function() {
     return promSatisfaction;
   }
 
-  function pointsJedi(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var ratings = generation.ratings;
+  function pointsJedi() {
     var jedi = 0;
     var promJedi = 0;
     for (var i = 0; i < ratings.length; i++) {
@@ -397,10 +352,7 @@ window.addEventListener('click', function() {
     return promJedi;
   }
 
-  function pointsTeacher(sedeValue, generationValue) {
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var ratings = generation.ratings;
+  function pointsTeacher() {
     var teacher = 0;
     var promTeacher = 0;
     for (var i = 0; i < ratings.length; i++) {
@@ -412,12 +364,7 @@ window.addEventListener('click', function() {
 
   // --STUDENTS--
 
-  function studentPerfil(sedeValue, generationValue) {
-    var studentActive = 0;
-    var sedes = data[sedeValue];
-    var generation = sedes[generationValue];
-    var students = generation['students'];
-
+  function studentPerfil() {
     for (var i = 0; i < students.length; i++) {
       var boxStudentsPerfil = document.createElement('div');
       boxStudentsPerfil.setAttribute('class', 'box-students');
